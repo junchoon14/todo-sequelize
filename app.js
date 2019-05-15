b702eb7c2cab671852cf9d41ffd0d9d6360a2cde
Alvis Hsu <junchoon14@gmail.com>
2019-05-15 11:27:35 +0800

    add mysql database

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
+const db = require('./models')
+const Todo = db.Todo
+const User = db.User
+
 const port = 3000
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
@@ -32,7 +36,11 @@ app.get('/users/register', (req, res) => {
 })
 // 註冊檢查
 app.post('/users/register', (req, res) => {
-  res.send('register')
+  User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  }).then(user => res.redirect('/'))
 })
 // 登出
 app.get('/users/logout', (req, res) => {
@@ -40,5 +48,6 @@ app.get('/users/logout', (req, res) => {
 })
 
 app.listen(port, () => {
+  db.sequelize.sync()
   console.log(`App is running on port ${port}!`)
 })
