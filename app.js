c6c6fe8a25747551f102c7b83564c1861841b84c
Alvis Hsu <junchoon14@gmail.com>
2019-05-19 23:21:27 +0800

    add home and user routers

--- app.js
+++ app.js
@@ -5,49 +5,22 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(methodOverride('_method'))
+
 const db = require('./models')
 const Todo = db.Todo
 const User = db.User
 
 const port = 3000
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
-
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(methodOverride('_method'))
-
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
-
-// 認證系統的路由
-// 登入頁面
-app.get('/users/login', (req, res) => {
-  res.render('login')
-})
-// 登入檢查
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
-// 註冊頁面
-app.get('/users/register', (req, res) => {
-  res.render('register')
-})
-// 註冊檢查
-app.post('/users/register', (req, res) => {
-  User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  }).then(user => res.redirect('/'))
-})
-// 登出
-app.get('/users/logout', (req, res) => {
-  res.send('logout')
-})
+app.use('/', require('./routes/home'))
+app.use('/todos', require('./routes/todo'))
+app.use('/users', require('./routes/user'))
 
 app.listen(port, () => {
-  db.sequelize.sync()
+  db.sequelize.sync({ force: true })
   console.log(`App is running on port ${port}!`)
 })
---
+++ routes/home.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+const Todo = require('../models/todo')
+
+router.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+module.exports = router
